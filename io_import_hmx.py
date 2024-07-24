45d0a0f29e876841bc39d69e288435a572121d68
vidsicious612 <ejake4545@gmail.com>
2024-07-24 18:20:41 -0400

    DC3 support

--- io_import_hmx.py
+++ io_import_hmx.py
@@ -275,21 +275,12 @@ class ImportMilo(Operator, ImportHelper):
                     filenames.append(b_numstring(f))
                 rest_file = f.read()
                 files = rest_file.split(b'\xAD\xDE\xAD\xDE')
-                for directory, name, file in zip(dirs, filenames, files):
-                    if ".mat" in name and "Mat" in directory:
-                        MatTexNames.append(name)
-                        MatTexFiles.append(file)
-                    if "Tex" in directory:
-                        if not "TexBlend" in directory:
-                            Tex(context, basename, self, name, file)
-                            MatTexNames.append(name)
-                            MatTexFiles.append(file)
-                    if ".mesh" in name and "Mesh" in directory:
-                        Mesh(self, name, file, basename, MatTexNames, MatTexFiles)
-                    if ".mesh" in name and "Trans" in directory:
-                        Trans(basename, name, file)
-                    if "TransAnim" in directory:
-                        TransAnim(file)
+                for file in files:
+                    header = file[:4]
+                    if header == b'\x00\x00\x00\x26':
+                        DC3Mesh(self, file)
+                    elif header == b'\x00\x00\x00\x0B':
+                        DC3Tex(self, file)
         return {'FINISHED'}                
 
 def Tex(context, basename, self, filename, file):
@@ -1616,6 +1607,142 @@ def Mesh(self, filename, file, basename, MatTexNames, MatTexFiles):
                             b = b_float(f)
                             a = b_float(f)
                             mat.diffuse_color = (r, g, b, a)                            
+def DC3Mesh(self, file):
+    f = io.BytesIO(file)
+    f.seek(21)
+    LocalTFM = struct.unpack('>12f', f.read(48))
+    WorldTFM = struct.unpack('>12f', f.read(48))
+    f.seek(4, 1)
+    Target = b_numstring(f)
+    f.seek(1, 1)
+    Parent = b_numstring(f)
+    f.seek(29, 1)
+    MatName = b_numstring(f)
+    MeshName = b_numstring(f)
+    if self.low_lod_setting:
+        if "lod01" in MeshName or "lod02" in MeshName:
+            return
+    if self.shadow_setting:
+        if "shadow" in MeshName:
+            return
+    f.seek(9, 1)
+    VertCount = b_int(f)
+    f.seek(9, 1)
+    Verts = []
+    UVs = []
+    for i in range(VertCount):
+        x, y, z = struct.unpack('>fff', f.read(12))
+        f.seek(4, 1)
+        u, v = struct.unpack('>ee', f.read(4))
+        f.seek(16, 1)
+        Verts.append((x, y, z))
+        UVs.append((u, v))
+    FaceCount = b_int(f)
+    Faces = []
+    for x in range(FaceCount):
+        Face = struct.unpack('>HHH', f.read(6))
+        Faces.append(Face)
+    GroupSizesCount = b_int(f)
+    for x in range(GroupSizesCount):
+        f.read(1)
+    BoneNames = []
+    BoneCount = b_int(f)
+    for x in range(BoneCount):
+        BoneName = b_numstring(f)
+        TFM = struct.unpack('>12f', f.read(48))
+        BoneNames.append(BoneName)
+    mesh = bpy.data.meshes.new(name=MeshName)
+    obj = bpy.data.objects.new(MeshName, mesh)
+    bpy.context.scene.collection.objects.link(obj)
+    obj.matrix_world = mathutils.Matrix((
+        (WorldTFM[0], WorldTFM[3], WorldTFM[6], WorldTFM[9],),
+        (WorldTFM[1], WorldTFM[4], WorldTFM[7], WorldTFM[10],),
+        (WorldTFM[2], WorldTFM[5], WorldTFM[8], WorldTFM[11],),
+        (0.0, 0.0, 0.0, 1.0),
+    ))
+    mesh.from_pydata(Verts, [], Faces)
+    mesh.use_auto_smooth = True
+    uv_layer = mesh.uv_layers.new(name="UVMap")
+    for face in mesh.polygons:
+        face.use_smooth = True
+        for loop_index in face.loop_indices:
+            vertex_index = mesh.loops[loop_index].vertex_index
+            uv = UVs[vertex_index]
+            flip = (uv[0], 1 - uv[1])
+            uv_layer.data[loop_index].uv = flip
+    mesh.update()
+    bpy.context.view_layer.objects.active = obj
+    if len(MatName) != 0:
+        mat = bpy.data.materials.get(MatName)
+        if mat is None:
+            mat = bpy.data.materials.new(name=MatName)
+        obj = bpy.context.active_object
+        if obj:
+            if obj.data.materials:
+                obj.data.materials[0] = mat
+            else:
+                obj.data.materials.append(mat)
+
+def DC3Tex(self, file):
+    try:
+        directory = os.path.dirname(self.filepath)
+        f = io.BytesIO(file)
+        f.seek(17)
+        Width = b_int(f)
+        Height = b_int(f)
+        f.seek(4, 1)
+        TexName = b_numstring(f)
+        TexName = TexName.strip('../')
+        TexName = TexName.replace('/', '_')
+        f.seek(16, 1)
+        Encoding = b_int(f)
+        MipMapCount = struct.unpack('>B', f.read(1))
+        MipMapCount = int.from_bytes(MipMapCount, byteorder='little')
+        EncodeOut = b''
+        if Encoding == 8:
+            EncodeOut = b'\x44\x58\x54\x31'
+        elif Encoding == 24:
+            EncodeOut = b'\x44\x58\x54\x35'
+        elif Encoding == 32:
+            EncodeOut = b'\x41\x54\x49\x32'
+        f.seek(21, 1)
+        BitmapOffset = f.tell()
+        Bitmap = f.read()
+        f.seek(BitmapOffset)
+        Pixels = []
+        for x in range(len(Bitmap)):
+            Pixel1 = f.read(1)
+            Pixel2 = f.read(1)
+            Pixels.append((Pixel2, Pixel1))
+        file_path = os.path.join(directory, TexName[:-4] + ".dds")
+        with open(file_path, 'wb') as output_file:
+            output_file.write(struct.pack('ccc', b'D', b'D', b'S',))
+            output_file.write(b'\x20')
+            output_file.write(struct.pack('I', 124))
+            output_file.write(struct.pack('I', 528391))
+            output_file.write(struct.pack('I', Height))
+            output_file.write(struct.pack('I', Width))
+            output_file.write(struct.pack('III', 0, 0, MipMapCount))
+            for x in range(11):
+                output_file.write(struct.pack('I', 0))
+            output_file.write(struct.pack('I', 32))
+            output_file.write(struct.pack('I', 4))
+            output_file.write(EncodeOut)
+            output_file.write(struct.pack('IIIII', 0, 0, 0, 0, 0))
+            output_file.write(struct.pack('I', 4096))
+            output_file.write(struct.pack('IIII', 0, 0, 0, 0))
+            for Pixel in Pixels:
+                byte1, byte2 = Pixel
+                output_file.write(byte1)
+                output_file.write(byte2)
+            
+        print("Exported texture:", file_path)
+
+        f.close()
+        del f
+    except Exception as e:
+        print(e)
+        
 def Trans(basename, filename, file):        
     f = io.BytesIO(file)
     # Hacky way to guess endian
