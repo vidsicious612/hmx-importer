4780b98adad5ddcc8974390607a042a49e1347ae
vidsicious612 <ejake4545@gmail.com>
2024-07-25 04:24:21 -0400

    Fix DC3 low LOD mesh importing

--- io_import_hmx.py
+++ io_import_hmx.py
@@ -1620,7 +1620,7 @@ def DC3Mesh(self, file):
     MatName = b_numstring(f)
     MeshName = b_numstring(f)
     if self.low_lod_setting:
-        if "lod01" in MeshName or "lod02" in MeshName:
+        if "lod" in MeshName
             return
     if self.shadow_setting:
         if "shadow" in MeshName:
