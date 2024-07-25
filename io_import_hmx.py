9d1f80be9d35875c74a9c6d139cbfc644d2a7d38
vidsicious612 <ejake4545@gmail.com>
2024-07-25 18:51:48 -0400

    Support for RB2 wii prototype + Wii lego

--- io_import_hmx.py
+++ io_import_hmx.py
@@ -81,9 +81,10 @@ class ImportMilo(Operator, ImportHelper):
     ) 
     
     GameItems = [
-        ('OPTION_A', 'LRB', ''),
-        ('OPTION_B', 'GDRB', ''),
-        ('OPTION_C', 'RB3', ''),
+        ('OPTION_A', 'RB2', ''),
+        ('OPTION_B', 'LRB', ''),
+        ('OPTION_C', 'GDRB', ''),
+        ('OPTION_D', 'RB3', ''),
     ]
         
     bpy.types.Scene.games = EnumProperty(
@@ -163,7 +164,7 @@ class ImportMilo(Operator, ImportHelper):
                                 MatTexNames.append(name)
                                 MatTexFiles.append(file)
                         if ".mesh" in name and "Mesh" in directory:
-                            Mesh(self, name, file, basename, MatTexNames, MatTexFiles)
+                            Mesh(self, context, name, file, basename, MatTexNames, MatTexFiles)
                         if "bone" in name and "Mesh" in directory:
                             Trans(basename, name, file)
                         if "TransAnim" in directory:
@@ -190,7 +191,7 @@ class ImportMilo(Operator, ImportHelper):
                                 MatTexNames.append(name)
                                 MatTexFiles.append(file)
                         if ".mesh" in name and "Mesh" in directory:
-                            Mesh(self, name, file, basename, MatTexNames, MatTexFiles)
+                            Mesh(self, context, name, file, basename, MatTexNames, MatTexFiles)
                         if ".mesh" in name and "Trans" in directory:
                             Trans(basename, name, file)            
                         if "TransAnim" in directory:
@@ -218,7 +219,7 @@ class ImportMilo(Operator, ImportHelper):
                                 MatTexNames.append(name)
                                 MatTexFiles.append(file)
                         if ".mesh" in name and "Mesh" in directory:
-                            Mesh(self, name, file, basename, MatTexNames, MatTexFiles)
+                            Mesh(self, context, name, file, basename, MatTexNames, MatTexFiles)
                         if ".mesh" in name and "Trans" in directory:
                             Trans(basename, name, file)
                         if "TransAnim" in directory:
@@ -257,7 +258,7 @@ class ImportMilo(Operator, ImportHelper):
                                 MatTexNames.append(name)
                                 MatTexFiles.append(file)
                         if ".mesh" in name and "Mesh" in directory:
-                            Mesh(self, name, file, basename, MatTexNames, MatTexFiles)
+                            Mesh(self, context, name, file, basename, MatTexNames, MatTexFiles)
                         if ".mesh" in name and "Trans" in directory:
                             Trans(basename, name, file)
                         if "TransAnim" in directory:
@@ -285,7 +286,7 @@ class ImportMilo(Operator, ImportHelper):
                                 MatTexNames.append(name)
                                 MatTexFiles.append(file)
                         if ".mesh" in name and "Mesh" in directory:
-                            Mesh(self, name, file, basename, MatTexNames, MatTexFiles)
+                            Mesh(self, context, name, file, basename, MatTexNames, MatTexFiles)
                         if ".mesh" in name and "Trans" in directory:
                             Trans(basename, name, file)
                         if "TransAnim" in directory:
@@ -446,7 +447,7 @@ def Tex(context, basename, self, filename, file):
                             output_file.write(byte2)
             
             print("Exported texture:", file_path)
-        if context.scene.games == 'OPTION_A':
+        if context.scene.games == 'OPTION_B':
             f.seek(17)
             # Extract width and height
             if LittleEndian == True:
@@ -534,7 +535,7 @@ def Tex(context, basename, self, filename, file):
                             output_file.write(byte2)
             
             print("Exported texture:", file_path)
-        elif context.scene.games == 'OPTION_B':
+        elif context.scene.games == 'OPTION_C':
             f.seek(18)
             # Extract width and height
             if LittleEndian == True:
@@ -622,7 +623,7 @@ def Tex(context, basename, self, filename, file):
                             output_file.write(byte2)
             
             print("Exported texture:", file_path)            
-        elif context.scene.games == 'OPTION_C':
+        elif context.scene.games == 'OPTION_D':
             f.seek(17)
             # Extract width and height
             if LittleEndian == True:
@@ -715,7 +716,7 @@ def Tex(context, basename, self, filename, file):
     except Exception as e:
         print(e)
         
-def Mesh(self, filename, file, basename, MatTexNames, MatTexFiles):
+def Mesh(self, context, filename, file, basename, MatTexNames, MatTexFiles):
     f = io.BytesIO(file)
     # Hacky way to guess endian
     Versions = [10, 13, 14, 22, 25, 28, 29, 34, 36, 37, 38]
@@ -1033,6 +1034,120 @@ def Mesh(self, filename, file, basename, MatTexNames, MatTexFiles):
                             b = l_float(f)
                             a = l_float(f)
                             mat.diffuse_color = (r, g, b, a)            
+    if Version == 34 and LittleEndian == True:
+        # RB2 prototype + Lego
+        if self.low_lod_setting:
+            if "lod01" in filename or "lod02" in filename:
+                return
+        if self.shadow_setting:
+            if "shadow" in filename:
+                return
+        f.seek(21)
+        LocalTFM = struct.unpack('12f', f.read(48))
+        WorldTFM = struct.unpack('12f', f.read(48))
+        f.seek(4, 1)
+        Target = l_numstring(f)
+        f.seek(1, 1)
+        Parent = l_numstring(f)
+        f.seek(25, 1)
+        MatName = l_numstring(f)
+        MeshName = l_numstring(f)
+        f.seek(9, 1)
+        VertCount = l_int(f)
+        Verts = []
+        Normals = []
+        Weights = []
+        UVs = []
+        Indices = []
+        for i in range(VertCount):
+            if context.scene.games == 'OPTION_A':
+                x, y, z = struct.unpack('fff', f.read(12))                
+                nx, ny, nz = struct.unpack('fff', f.read(12))
+                w1, w2, w3, w4 = struct.unpack('ffff', f.read(16))
+                u, v = struct.unpack('ff', f.read(8))
+                b1, b2, b3, b4 = struct.unpack('HHHH', f.read(8))
+                f.seek(16, 1)
+                Verts.append((x, y, z))
+                Normals.append((nx, ny, nz))
+                Weights.append((w1, w2, w3, w4))
+                UVs.append((u, v))
+                Indices.append((b1, b2, b3, b4))
+            elif context.scene.games == 'OPTION_B':
+                x, y, z, w = struct.unpack('ffff', f.read(16))                
+                nx, ny, nz, nw = struct.unpack('ffff', f.read(16))
+                w1, w2, w3, w4 = struct.unpack('ffff', f.read(16))
+                u, v = struct.unpack('ff', f.read(8))
+                b1, b2, b3, b4 = struct.unpack('HHHH', f.read(8))
+                f.seek(16, 1)
+                Verts.append((x, y, z))
+                Normals.append((nx, ny, nz))
+                Weights.append((w1, w2, w3, w4))
+                UVs.append((u, v))
+                Indices.append((b1, b2, b3, b4))
+        FaceCount = l_int(f)
+        Faces = []
+        for x in range(FaceCount):
+            Face = struct.unpack('HHH', f.read(6))
+            Faces.append(Face)
+        GroupSizesCount = l_int(f)
+        for x in range(GroupSizesCount):
+            f.read(1)
+        BoneNames = []
+        BoneCount = l_int(f)
+        for x in range(BoneCount):
+            BoneName = b_numstring(f)
+            TFM = struct.unpack('12f', f.read(48))
+            BoneNames.append(BoneName)
+        mesh = bpy.data.meshes.new(name=filename)
+        obj = bpy.data.objects.new(filename, mesh)
+        bpy.context.scene.collection.objects.link(obj)
+        obj.matrix_world = mathutils.Matrix((
+            (WorldTFM[0], WorldTFM[3], WorldTFM[6], WorldTFM[9],),
+            (WorldTFM[1], WorldTFM[4], WorldTFM[7], WorldTFM[10],),
+            (WorldTFM[2], WorldTFM[5], WorldTFM[8], WorldTFM[11],),
+            (0.0, 0.0, 0.0, 1.0),
+        ))
+        mesh.from_pydata(Verts, [], Faces)
+        mesh.use_auto_smooth = True
+        uv_layer = mesh.uv_layers.new(name="UVMap")
+        for face in mesh.polygons:
+            face.use_smooth = True
+            for loop_index in face.loop_indices:
+                vertex_index = mesh.loops[loop_index].vertex_index
+                uv = UVs[vertex_index]
+                flip = (uv[0], 1 - uv[1])
+                uv_layer.data[loop_index].uv = flip
+        mesh.update()
+        bpy.ops.object.select_all(action='DESELECT')
+        obj.select_set(True)
+        bpy.context.view_layer.objects.active = obj
+        try:
+            for i, vertex in enumerate(mesh.vertices):
+                if i < len(Indices):
+                    group_names = [BoneNames[idx] for idx in Indices[i]]
+                    group_weights = Weights[i]
+                            
+                    for group_name, weight in zip(group_names, group_weights):
+                        if group_name not in obj.vertex_groups:
+                            obj.vertex_groups.new(name=group_name)
+                        group_index = obj.vertex_groups[group_name].index
+                        obj.vertex_groups[group_index].add([vertex.index], weight, 'REPLACE')
+            mesh.update()
+            print("Bone weights assigned to:", obj.name)                
+        except IndexError:
+            print("Invalid weights")
+        obj.select_set(False)
+        bpy.context.view_layer.objects.active = obj
+        if len(MatName) != 0:
+            mat = bpy.data.materials.get(MatName)
+            if mat is None:
+                mat = bpy.data.materials.new(name=MatName)
+            obj = bpy.context.active_object
+            if obj:
+                if obj.data.materials:
+                    obj.data.materials[0] = mat
+                else:
+                    obj.data.materials.append(mat)
     if Version == 34 and BigEndian == True:
         if self.low_lod_setting:
             if "lod01" in filename or "lod02" in filename:
